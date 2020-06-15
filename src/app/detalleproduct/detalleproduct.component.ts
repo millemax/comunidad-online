9cfb702bc7ee1de8c202bb4cde103ec6f775b965
millermax <romel_23_3@hotmail.com>
2020-06-15 12:49:45 -0500

    detalles de producto todavia estatico

--- src/app/detalleproduct/detalleproduct.component.ts
+++ src/app/detalleproduct/detalleproduct.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+//estamos importando a la casa de la moneda
+import { StarRatingComponent } from 'ng-starrating';
 
 @Component({
   selector: 'app-detalleproduct',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleproductComponent implements OnInit {
 
-  constructor() { }
+  idProducto:string;
+
+
+  constructor( private _route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.obtenerid();
+
   }
+
+  obtenerid(){
+    console.log("el id del producto detalles", this._route.snapshot.paramMap.get('id'))
+    this.idProducto=this._route.snapshot.paramMap.get('id');
+
+  }
+
+   //esto recibe los puntajes de la estrella
+onRate($event:{oldValue:number, newValue:number, starRating:StarRatingComponent}) {
+    alert(`Old Value:${$event.oldValue}, 
+    New Value: ${$event.newValue}, 
+    Checked Color: ${$event.starRating.checkedcolor}, 
+    Unchecked Color: ${$event.starRating.uncheckedcolor}`);
+
+}
+
+obtenerproducto(){
+  
+}
 
 }
